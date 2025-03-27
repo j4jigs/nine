b1b66c39d1041402f459032ae713fa915f81bfc6
jignesharies <jignesh@aries.com>
2025-03-27 22:17:33 +0530

    variable fix

--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
 variable "region" {
+  type    = string
   default = "us-east-1"
 }
 
 variable "s3_bucket_name" {
+  type    = string
   default = "github-cicd-demo-bucket-123456"
 }
 
 variable "ami" {
-  default = "ami-071226ecf16aa7d96" # Amazon Linux 2 (us-east-1)
+  type    = string
+  default = "ami-0c02fb55956c7d316"
 }
 
 variable "instance_type" {
+  type    = string
   default = "t2.micro"
 }
