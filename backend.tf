733e42f5ece5b14f8122fcd570feff995f64a22d
jignesharies <jignesh@aries.com>
2025-03-27 22:43:03 +0530

    backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket-1234567"
-    key            = "envterraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    }
+    bucket  = "my-terraform-state-bucket-1234567"
+    key     = "envterraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
 }
