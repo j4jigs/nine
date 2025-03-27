45faa6eaf2fbf2d4df24203dd656de1979c3d441
jignesharies <jignesh@aries.com>
2025-03-27 22:56:22 +0530

    vpc updated

--- main.tf
+++ main.tf
@@ -14,10 +14,10 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket        = var.s3_bucket_name
-  force_destroy = true
-}
+#resource "aws_s3_bucket" "my_bucket" {
+#  bucket        = var.s3_bucket_name
+#  force_destroy = true
+#}
 
 resource "aws_instance" "my_ec2" {
   ami           = var.ami
