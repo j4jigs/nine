a05ec96fa05f96966b5264c37150bf943b9c790f
jignesharies <jignesh@aries.com>
2025-03-27 22:31:06 +0530

    main.tf formated

--- main.tf
+++ main.tf
@@ -41,9 +41,9 @@ resource "aws_vpc" "main" {
 
 # 2. Public Subnet
 resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
