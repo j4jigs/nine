84539dc6e308fb697333c370a9c27feeb3e07846
jignesharies <jignesh@aries.com>
2025-03-28 10:12:14 +0530

    rds instance added in private subnet formating

--- rds.tf
+++ rds.tf
@@ -44,11 +44,11 @@ resource "aws_db_instance" "mysql" {
   allocated_storage       = 20
   storage_type            = "gp2"
   username                = "admin"
-  password                = "MySecurePass123!"  # Use sensitive variable or Secrets Manager in real-world
+  password                = "MySecurePass123!" # Use sensitive variable or Secrets Manager in real-world
   db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
   skip_final_snapshot     = true
-  publicly_accessible     = false  # Important: must be false for private subnet
+  publicly_accessible     = false # Important: must be false for private subnet
   multi_az                = false
   backup_retention_period = 7
 
