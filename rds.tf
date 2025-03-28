00cff532d4681f586661db8d953629a1a5bb6bbe
jignesharies <jignesh@aries.com>
2025-03-28 10:29:16 +0530

    subnet updated

--- rds.tf
+++ rds.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "private1" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.3.0/24"
+  cidr_block              = "10.0.5.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = false
 
