6fbb753cb19e606d4a374e51ccef085cea9374c3
jignesharies <jignesh@aries.com>
2025-03-28 10:16:36 +0530

    subnets added

--- rds.tf
+++ rds.tf
@@ -1,3 +1,26 @@
+resource "aws_subnet" "private1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
+
+
 # 1. Create Security Group for RDS (MySQL Port)
 resource "aws_security_group" "rds_sg" {
   name        = "rds-mysql-sg"
